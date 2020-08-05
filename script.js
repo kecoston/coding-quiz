1a64dfde9a0ff7a9e9b716574d9a2eaff049e8ab
KSherazi <kecoston@gmail.com>
2020-08-04 21:42:53 -0500

    first question functions

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-var startButton = document.querySelectorAll("#answerChoices");
+var startButton = document.querySelectorAll("button");
 var questionEl = document.querySelectorAll("p");
-var answerEl = document.querySelectorAll("answerChoices");
+var answerEl = document.getElementById("answerChoices");
+
+var partsOfQuestions = ["question", "option1", "option2", "option3", "option4", "answer"]
+
 
 // The array of question objects for the game 
 var questionAnswer = [
-    {question: "Commonly used data types DO NOT include:", 
-    answer1:"1. Strings",
-    answer2:"2. Booleans",
-    answer3:"3. Alerts",
-    answer4:"4. Numbers"  },
+    {"question1" : "Commonly used data types DO NOT include:", 
+    "option1":"1. Strings",
+    "option2":"2. Booleans",
+    "option3":"3. Alerts",
+    "option4":"4. Numbers",
+    "answer":"option2"  },
 
-    {question: "The condition in an if/else statement is enclosed within:", 
-    answer1:"1. Quotes",
-    answer2:"2. Curly Brackets",
-    answer3:"3. Parentheses",
-    answer4:"4. Square Brackets" },
+    {"question": "The condition in an if/else statement is enclosed within:", 
+    "option1":"1. Quotes",
+    "option2":"2. Curly Brackets",
+    "option3":"3. Parentheses",
+    "option4":"4. Square Brackets",
+    "answer": "option3" },
     
-    {question: "Arrays in Javascript can be used to store:", 
-    answer1:"1. Numbers and Strings",
-    answer2:"2. Other Arrays",
-    answer3:"3. Booleans",
-    answer4:"4. All of the above," },
+    {"question": "Arrays in Javascript can be used to store:", 
+    "option1":"1. Numbers and Strings",
+    "option2":"2. Other Arrays",
+    "option3":"3. nBooleans",
+    "option4":"4. All of the above,",
+    "answer": "option4" },
     
-    {question: "String values must be enclosed within ____ when being assigned to variables:", 
-    answer1:"1. Commas",
-    answer2:"2. Curly Brackets",
-    answer4:"3. Quotes",
-    answer5:"4. Parentheses" },
+    {"question": "String values must be enclosed within ____ when being assigned to variables:", 
+    "option1":"1. Commas",
+    "option2":"2. Curly Brackets",
+    "option4":"3. Quotes",
+    "option5":"4. Parentheses",
+    "answer": "option2" },
     
-    {question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
-    answer1:"1. JavaScript",
-    answer2:"2. Terminal/Bash",
-    answer3:"3. For Loops",
-    answer4:"4. Console.Log" },
+    {"question": "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    "option1":"1. JavaScript",
+    "option2":"2. Terminal/Bash",
+    "option3":"3. For Loops",
+    "option4":"4. Console.Log",
+    "answer": "option3" },
 ];
 
+
+// //pulls all five objects
+for (var i = 0; i < questionAnswer.length; i++){
+    console.log(questionAnswer)
+}
+
+//pulls first question
+console.log(JSON.stringify(questionAnswer[0]))
+
+// // Function pulls first question
 function startGame () {
 firstQuestion = questionAnswer.shift();
 console.log(firstQuestion)
-}
-
+};
 
 startGame();
 
